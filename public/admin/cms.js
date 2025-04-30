b4f37930ef0841d77c210a72cb7f1d0bf64e13f2
Justin Johnson <jj@mac.mynetworksettings.com>
2025-04-29 23:10:56 -0600

    Wrap try catch block in event listener

--- public/admin/cms.js
+++ public/admin/cms.js
@@ -1,15 +1,17 @@
-try {
-  console.log(process.env.AUTH0_CLIENT_ID);
-  console.log(process.env.AUTH0_DOMAIN);
-  const authProvider = window.DecapCmsLibAuth.createAuthProvider({
-    auth_url: `https://${process.env.AUTH0_DOMAIN}/authorize`,
-    app_id: `${process.env.AUTH0_CLIENT_ID}`,
-    auth_origin: "https://apxnr.netlify.app/admin/",
-    auth_type: "pkce",
-    scope: "openid profile email",
-  });
-  window.CMS.registerBackend("github", authProvider);
-  window.CMS.init();
-} catch (error) {
-  console.error("Error initializing Decap CMS:", error);
-}
+document.addEventListener("DOMContentLoaded", () => {
+  try {
+    console.log(process.env.AUTH0_CLIENT_ID);
+    console.log(process.env.AUTH0_DOMAIN);
+    const authProvider = window.DecapCmsLibAuth.createAuthProvider({
+      auth_url: `https://${process.env.AUTH0_DOMAIN}/authorize`,
+      app_id: `${process.env.AUTH0_CLIENT_ID}`,
+      auth_origin: "https://apxnr.netlify.app/admin/",
+      auth_type: "pkce",
+      scope: "openid profile email",
+    });
+    window.CMS.registerBackend("github", authProvider);
+    window.CMS.init();
+  } catch (error) {
+    console.error("Error initializing Decap CMS:", error);
+  }
+});
