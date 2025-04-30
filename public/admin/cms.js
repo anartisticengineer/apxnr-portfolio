94455230f975ce485c12239320bd889744edbd82
Justin Johnson <jj@mac.mynetworksettings.com>
2025-04-29 23:03:31 -0600

    Add error handling to cms js for troubleshooting

--- public/admin/cms.js
+++ public/admin/cms.js
@@ -1,10 +1,15 @@
-const authProvider = window.DecapCmsLibAuth.createAuthProvider({
-  auth_url: `https://${process.env.AUTH0_DOMAIN}/authorize`,
-  app_id: `${process.env.AUTH0_CLIENT_ID}`,
-  auth_origin: "https://apxnr.netlify.app/admin/",
-  auth_type: "pkce",
-  scope: "openid profile email",
-});
-
-window.CMS.registerBackend("github", authProvider);
-window.CMS.init();
+try {
+  console.log(process.env.AUTH0_CLIENT_ID);
+  console.log(process.env.AUTH0_DOMAIN);
+  const authProvider = window.DecapCmsLibAuth.createAuthProvider({
+    auth_url: `https://${process.env.AUTH0_DOMAIN}/authorize`,
+    app_id: `${process.env.AUTH0_CLIENT_ID}`,
+    auth_origin: "https://apxnr.netlify.app/admin/",
+    auth_type: "pkce",
+    scope: "openid profile email",
+  });
+  window.CMS.registerBackend("github", authProvider);
+  window.CMS.init();
+} catch (error) {
+  console.error("Error initializing Decap CMS:", error);
+}
