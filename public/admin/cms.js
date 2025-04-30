52990c14ae463a5a46e07436ee5e14a5079dfade
Justin Johnson <jj@mac.mynetworksettings.com>
2025-04-29 18:22:00 -0600

    revert to regular index html

--- public/admin/cms.js
+++ public/admin/cms.js
@@ -1,19 +1,10 @@
-/* eslint-disable no-undef */
-window.addEventListener("load", () => {
-  const script = document.createElement("script");
-  script.src =
-    "https://cdn.jsdelivr.net/npm/decap-cms-lib-auth@3.0.5/dist/decap-cms-lib-auth.js";
-  script.onload = () => {
-    window.CMS.registerBackend(
-      "github",
-      DecapCmsLibAuth.createAuthProvider({
-        auth_url: `https://${AUTH0_DOMAIN}/authorize`,
-        app_id: `${AUTH0_CLIENT_ID}`,
-        auth_origin: "https://apxnr.netlify.app/admin/",
-        auth_type: "pkce",
-        scope: "openid profile email",
-      })
-    );
-  };
-  document.body.appendChild(script);
+const authProvider = window.DecapCmsLibAuth.createAuthProvider({
+  auth_url: `https://${process.env.AUTH0_DOMAIN}/authorize`,
+  app_id: `${process.env.AUTH0_CLIENT_ID}`,
+  auth_origin: "https://apxnr.netlify.app/admin/",
+  auth_type: "pkce",
+  scope: "openid profile email",
 });
+
+window.CMS.registerBackend("github", authProvider);
+window.CMS.init();
