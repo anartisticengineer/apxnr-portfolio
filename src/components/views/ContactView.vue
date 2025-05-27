ed5d6dc68d06d57c85f8fb2b4b62f4d01d771844
Justin Johnson <jj@mac.mynetworksettings.com>
2025-05-27 11:53:15 -0600

    Added form validation

--- src/components/views/ContactView.vue
+++ src/components/views/ContactView.vue
@@ -3,9 +3,15 @@
     <h1 class="text-left text-md-center w-100 ma-5">Contact</h1>
     <v-divider color="accent" />
     <div class="d-flex flex-column flex-md-row ma-5">
-      <v-form class="ma-5 w-md-50" validate-on="blur">
+      <v-form
+        class="ma-5 w-md-50"
+        validate-on="blur"
+        @submit="handleSubmit"
+        ref="form"
+      >
         <v-text-field
           v-model="name"
+          :rules="nameRules"
           label="Name"
           variant="outlined"
           class="py-2"
@@ -13,6 +19,7 @@
         ></v-text-field>
         <v-text-field
           v-model="email"
+          :rules="emailRules"
           label="Email"
           type="email"
           variant="outlined"
@@ -21,11 +28,18 @@
         ></v-text-field>
         <v-select
           label="Inquiry Type"
-          :items="['Commissions', 'Job Offers', 'Other/Just saying hi ;)']"
+          v-model="inquiryType"
+          :rules="inquiryTypeRules"
+          :items="[
+            'Commissions',
+            'Job Opportunities',
+            'Other/Just saying hi ;)',
+          ]"
           variant="outlined"
         ></v-select>
         <v-textarea
           v-model="message"
+          :rules="messageRules"
           label="Message"
           variant="outlined"
           class="py-2"
@@ -41,7 +55,32 @@
     </div>
   </v-container>
 </template>
-<script>
+<script setup lang="ts">
+import { ref } from "vue";
+//form reference
+const form = ref();
+//form elements and rules
+const name = ref("");
+const nameRules = [(v: string) => !!v || "Name is required"];
+const email = ref("");
+const emailRules = [
+  (v: string) => !!v || "E-mail is required",
+  (v: string) => /.+@.+\..+/.test(v) || "E-mail must be valid",
+];
+const inquiryType = ref("");
+const inquiryTypeRules = [(v: string) => !!v || "Inquiry type is required"];
+const message = ref("");
+const messageRules = [(v: string) => !!v || "Message is required"];
+//submit form elements
+const handleSubmit = (e: Event) => {
+  e.preventDefault();
+  if (form.value.isValid) {
+    alert("Form submitted");
+  }
+};
+</script>
+
+<script lang="ts">
 export default {
   name: "ContactView",
 };
