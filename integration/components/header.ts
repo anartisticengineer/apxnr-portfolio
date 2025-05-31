9a9199febf49d06f53fe263ab87ccaec0141d43c
Justin Johnson <jj@mac.mynetworksettings.com>
2025-05-31 09:49:56 -0600

    Add tests for desktop navigation

--- integration/components/header.ts
+++ integration/components/header.ts
@@ -6,6 +6,12 @@ export default class Header {
   readonly mobileMenuButton: Locator;
   readonly desktopHeaderLinks: Locator;
   readonly mobileHeaderLinks: Locator;
+  readonly homeLink: Locator;
+  readonly desktopAboutLink: Locator;
+  readonly desktopContactLink: Locator;
+  readonly desktopProjectToggle: Locator;
+  readonly mobileAboutLink: Locator;
+  readonly mobileContactLink: Locator;
   constructor(page: Page) {
     this.page = page;
     this.headerContainer = this.page.getByTestId("header");
@@ -17,6 +23,23 @@ export default class Header {
     this.mobileHeaderLinks = this.headerContainer.getByTestId(
       "mobile-header-links"
     );
+    this.homeLink = this.headerContainer.getByRole("link", {
+      name: "Logo for header",
+    });
+    this.desktopAboutLink = this.desktopHeaderLinks.getByRole("link", {
+      name: "About Me",
+    });
+    this.desktopContactLink = this.desktopHeaderLinks.getByRole("link", {
+      name: "Contact",
+    });
+    this.desktopProjectToggle =
+      this.headerContainer.getByTestId("project-toggle");
+    this.mobileAboutLink = this.mobileHeaderLinks.getByRole("link", {
+      name: "About Me",
+    });
+    this.mobileContactLink = this.mobileHeaderLinks.getByRole("link", {
+      name: "Contact",
+    });
   }
 
   async openMobileMenu() {
@@ -29,5 +52,25 @@ export default class Header {
 
   async isMobile() {
     return this.mobileMenuButton.isVisible();
+  }
+
+  async goHome() {
+    await this.homeLink.click();
+  }
+
+  async goToAboutPage() {
+    if (await this.isMobile()) {
+      await this.mobileAboutLink.click();
+      return;
+    }
+    await this.desktopAboutLink.click();
+  }
+
+  async goToContactPage() {
+    if (await this.isMobile()) {
+      await this.mobileContactLink.click();
+      return;
+    }
+    await this.desktopContactLink.click();
   }
 }
--- integration/tests/header/header.desktop.spec.ts
+++ integration/tests/header/header.desktop.spec.ts
@@ -6,5 +6,15 @@ test.describe("Desktop Header Navigation", () => {
     await expect(homePage.header.desktopHeaderLinks).toBeVisible();
   });
 
+  test("Go to About Me Page", async ({ homePage }) => {
+    await homePage.header.goToAboutPage();
+    expect(homePage.getUrl()).toContain("/about-me");
+  });
+
+  test("Go to Contact Page", async ({ homePage }) => {
+    await homePage.header.goToContactPage();
+    expect(homePage.getUrl()).toContain("/contact");
+  });
+
   test.skip("Open Project Links", async ({ homePage }) => {});
 });
