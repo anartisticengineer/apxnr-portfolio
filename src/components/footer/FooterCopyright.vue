9e4478b3b7a566c35e9a6f0ee5bf696869650d10
Justin Johnson <jj@mac.mynetworksettings.com>
2025-06-13 15:01:42 -0600

    Add kofi donation button and format fixes

---
+++ src/components/footer/FooterCopyright.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    :id="`footer-copyright-${isMobile ? 'mobile' : 'desktop'}`"
+    class="text-center my-0 my-md-5"
+  >
+    <strong>&copy; APXNR</strong> - {{ new Date().getFullYear() }}
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "FooterCopyright",
+  props: {
+    isMobile: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
